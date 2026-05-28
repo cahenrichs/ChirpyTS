267da991a3748c2363ce7bb5f60e96fd4a9ddebf
Clint <cahenrichs@gmail.com>
2026-05-28 08:34:51 -0500

    Moved around the code to make more sense.

---
+++ src/api/reset.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+import { config } from "../config.js";
+
+export async function handlerReset(req: Request, res: Response) {
+  config.fileserverHits = 0;
+  res.write("Hits reset to 0");
+  res.end();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,20 @@
-import express, { Request, Response, NextFunction } from "express";
-import { config } from "./config.js"
-import { nextTick } from "node:process";
+import { handlerReset } from "./api/reset.js";
+import { handlerReadiness } from "./api/readiness.js";
+import { middlewareMetricsInc, middlewareLogResponse } from "./api/middleware.js";
+import { handlerMetrics } from "./api/metrics.js";
+import express  from "express";
 
 const app = express()
 const PORT = 8080
 
-const handlerReadiness = (req: Request, res: Response) => {
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send("OK");
-};
-
-type Middleware = (req: Request, res: Response, next: NextFunction) => void; 
-
-export const middlewareLogResponses: Middleware = (req, res, next) => {
-    res.on("finish", () => {
-      const statusCode = res.statusCode;
-      if (statusCode < 200 || statusCode >= 300) {
-        console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
-      };
-});
-next();
-}
-
-export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
-    config.fileserverHits ++
-    next();
-}
-
-export function numberofRequestResponse(req: Request, res: Response) {
-    const count = config.fileserverHits
-    res.set("Content-Type", "text/plain");
-    res.send(`Hits: ${count}`)
-}
-
-export function resetFileserverHits(req: Request, res: Response, next: NextFunction) {
-    config.fileserverHits = 0
-    res.send("OK");
-    next();
-}
-
-app.use(middlewareLogResponses)
+app.use(middlewareLogResponse)
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
-app.get("/api/healthz", middlewareMetricsInc, handlerReadiness);
-app.get("/api/metrics", numberofRequestResponse)
-app.get("/api/reset", resetFileserverHits)
+
+
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics)
+app.get("/api/reset", handlerReset)
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
