edadac818dadddab98eb8f6ddf3042f6fa051e7e
Clint <cahenrichs@gmail.com>
2026-06-02 16:33:33 -0500

    Got drizzle and the db set up.

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,21 @@
 
+process.loadEnvFile();
 
 type APIConfig = {
     fileserverHits: number,
+    dbURL: string
 }
 
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL")
+
 };
+
+function envOrThrow(key: string): string {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Missing environment varible: ${key}`);
+    }
+    return value;
+}
