20d84fce5a6f0461431083364107749aa1f73487
Clint <cahenrichs@gmail.com>
2026-06-02 22:33:35 -0500

    rearanged file and made a MasterConfig type

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,33 @@
+import type { MigrationConfig } from "drizzle-orm/migrator";
 
 process.loadEnvFile();
 
 type APIConfig = {
     fileserverHits: number,
-    dbURL: string
 }
 
-export const config: APIConfig = {
-    fileserverHits: 0,
-    dbURL: envOrThrow("DB_URL")
+const migrationConfig: MigrationConfig = {
+  migrationsFolder: "./src/db/migrations",
+};
 
+type DbConfig = {
+  migrationConfig: MigrationConfig,
+  dbURL: string
+}
+
+type MasterConfig = {
+    api: APIConfig,
+    db: DbConfig
+}
+
+export const config: MasterConfig = {
+    api: {
+        fileserverHits: 0,
+    },
+    db: {
+        migrationConfig,
+        dbURL: envOrThrow("DB_URL")
+    }
 };
 
 function envOrThrow(key: string): string {
